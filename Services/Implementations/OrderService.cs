5ef9ccb514b4d28cf35b8d8af9fd4e7b3c34977c
ATartarus <kolyapryazko@gmail.com>
2024-03-17 20:43:56 +0300

    init

---
+++ Services/Implementations/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            return _context.Orders.OrderByDescending(order => order.Price * order.Quantity).FirstAsync();
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return _context.Orders.Where(order => order.Quantity > 10).ToListAsync();
+        }
+    }
+}
